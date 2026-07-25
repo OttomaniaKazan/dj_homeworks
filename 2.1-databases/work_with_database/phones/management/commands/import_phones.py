d163b74d5c0af9370a27ee1a11544151311b34f6
Evgeniy Titov <titevg@gmail.com>
2026-07-25 19:46:45 +0300

    homework 2.1 work_with_database

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,5 +1,8 @@
 import csv
-
+from decimal import Decimal
+from datetime import datetime
+from django.utils import timezone
+from django.utils.text import slugify
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
@@ -9,9 +12,30 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        with open('phones.csv', 'r', encoding='utf-8') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            phone_id = int(phone['id'])
+            name = phone['name']
+            image = phone['image']
+            price = Decimal(phone['price'].replace(',', '.'))
+            
+            # Создаем aware datetime с временной зоной по умолчанию
+            naive_datetime = datetime.strptime(phone['release_date'], '%Y-%m-%d')
+            release_date = timezone.make_aware(naive_datetime).date()
+            
+            lte_exists = phone['lte_exists'].strip().lower() == 'true'
+            slug = slugify(name)
+
+            Phone.objects.create(
+                id=phone_id,
+                name=name,
+                image=image,
+                price=price,
+                release_date=release_date,
+                lte_exists=lte_exists,
+                slug=slug
+            )
+
+        self.stdout.write(self.style.SUCCESS('Данные из phones.csv успешно импортированы в модель Phone.'))
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    # id, name, price, image, release_date, lte_exists и slug. Поле id — должно быть основным ключом модели.
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    image = models.URLField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    release_date = models.DateField(blank=True, null=True)
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
