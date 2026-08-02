2b922a661d8811d9f07b20f108207fca6b6781b5
Evgeniy Titov <titevg@gmail.com>
2026-08-02 21:49:19 +0300

    task orm_migrations completed

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -15,7 +15,8 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    # Меняем ForeignKey на ManyToManyField, переименовываем в teachers
+    teachers = models.ManyToManyField(Teacher, related_name='students', verbose_name='Учителя')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,12 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
 from .models import Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
+    
+    # Добавляем prefetch_related для оптимизации загрузки учителей
+    students = Student.objects.order_by('group').prefetch_related('teachers')
+    
+    context = {'object_list': students}
     return render(request, template, context)
