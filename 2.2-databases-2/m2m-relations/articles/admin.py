e78ecc4e1a19f221f9739fb97155231be69e63af
Evgeniy Titov <titevg@gmail.com>
2026-08-09 14:37:46 +0300

    m2m-relations completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        
+        main_count = 0
+        for form in self.forms:
+            # Проверяем, что форма прошла валидацию и не помечена на удаление (чекбокс DELETE)
+            if form.cleaned_data and not form.cleaned_data.get('DELETE', False):
+                if form.cleaned_data.get('is_main'):
+                    main_count += 1
+        
+        # Если основных разделов не ровно 1, выбрасываем ошибку
+        if main_count != 1:
+            raise ValidationError('У статьи должен быть указан ровно один основной раздел.')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1  # Количество пустых форм для добавления новых связей
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-
 from articles.models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
+
+    # Получаем статьи, отсортированные по дате публикации.
+    # prefetch_related('scopes__tag') загружает все связанные разделы и теги 
+    # за один дополнительный запрос, что сильно ускоряет работу страницы.
+    articles = Article.objects.order_by(ordering).prefetch_related('scopes__tag')
+
+    context = {
+        'object_list': articles
+    }
 
     return render(request, template, context)
